931c7bb96588f95da9c1fc8cf90018d110ff765b
ChristianDBriones <145032845+ChristianDBriones@users.noreply.github.com>
2024-01-02 20:55:14 -0600

    firstdraft

---
+++ Develop/index.js
@@ -0,0 +1,32 @@
+var inquirer = require('inquirer');
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+ 
+inquirer
+  .prompt([
+    /* Pass your questions in here */ "What is a tyrannasaurus rex?"
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
+
+// Function call to initialize app
+init();
