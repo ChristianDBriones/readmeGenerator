e8e2331abb59700afaad959c1ee9b4b08e27c3a5
ChristianDBriones <145032845+ChristianDBriones@users.noreply.github.com>
2024-01-04 16:49:57 -0600

    added some questions

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 var inquirer = require('inquirer');
+var fs = require('fs');
+var path = require('path')
+var generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    message: "What is the title of your Project?",
+    name: "title"
+  },
+  {
+    message: "What is the description of your Project?",
+    name: "description"
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFile(path.join(process.cwd(),fileName), data, (err) =>
+    err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
 function init() {
  
 inquirer
-  .prompt([
-    /* Pass your questions in here */ "What is a tyrannasaurus rex?"
-  ])
+  .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
     console.log(answers)
+    return writeToFile("output/README.md",generateMarkdown({...answers}))
   })
   .catch((error) => {
     if (error.isTtyError) {
---
+++ output/README.md
@@ -0,0 +1,2 @@
+# herer we go
+
